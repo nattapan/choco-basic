b8ef16ff8815dd3be054072ad716f1be3dfe5f44
nattapan <3727102+nattapan@users.noreply.github.com>
2021-12-21 14:12:40 +0700

    Add files via upload

---
+++ choco-basic.cmd
@@ -0,0 +1,36 @@
+choco install vcredist-all 7zip firefox googlechrome notepadplusplus dbeaver  winscp putty -y
+choco install adobereader  -y
+choco install googlechrome  -y
+choco install chocolatey-core.extension --version 1.3.5.1 -y
+choco install firefox  -y
+choco install chocolatey-windowsupdate.extension --version 1.0.4 -y
+choco install notepadplusplus.install  -y
+choco install 7zip.install  -y
+choco install dotnetfx --version 4.8.0.20190930 -y
+choco install git.install --version 2.34.1 -y
+choco install chocolatey-dotnetfx.extension --version 1.0.1 -y
+choco install microsoft-edge  -y
+choco install sysinternals --version 2021.12.16 -y
+choco install autohotkey --version 1.1.33.10 -y
+choco install putty.install --version 0.76 -y
+choco install gimp --version 2.10.28 -y
+choco install pdfcreator --version 4.3.0 -y
+choco install wget --version 1.21.2 -y
+choco install paint.net --version 4.3.4 -y
+choco install curl --version 7.80.0 -y
+choco install treesizefree --version 4.5.3 -y
+choco install dotnet3.5 --version 3.5.20160716 -y
+choco install nxlog --version 2.10.2150 -y
+choco install crystalreports2008runtime --version 12.4.1 -y
+choco install putty --version 0.76 -y
+choco install inkscape --version 1.1.1 -y
+choco install winscp --version 5.19.5 -y
+choco install sqlserver-odbcdriver --version 17.8.1.1 -y
+choco install chocolateygui --version 0.19.0 -y
+choco install procexp --version 16.43 -y
+choco install youtube-dl --version 2021.12.17 -y
+choco install autohotkey.install --version 1.1.33.10 -y
+choco install sql-server-express --version 2019.20200409 -y
+choco install vmware-tools --version 11.3.5.18557794 -y
+choco install advanced-ip-scanner --version 2.5.3850 -y
+choco install sql-server-management-studio --version 15.0.18390.0 -y
